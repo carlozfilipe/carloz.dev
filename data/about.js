97d7a4f739083f3d0e36d8a9ca1ee799b893b903
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-12-21 22:43:44 -0300

    Add Zeno Rocha template

---
+++ data/about.js
@@ -0,0 +1,11 @@
+const items = [
+  {
+    jobTitle: 'Full Stack Developer Jr.',
+    company: 'Autônomo',
+    companyUrl: 'https://carloz.dev',
+    startDate: '2022-01-01',
+    location: 'São Luís, Maranhão',
+  }
+]
+
+export default items
